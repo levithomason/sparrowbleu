861f2a08a835571043c163f5949b0f03161bb60e
Levi Thomason <me@levithomason.com>
2013-09-28 22:18:33 -0700

    gallery detail, workin on new galler image field

--- apps/sparrow_bleu/urls.py
+++ apps/sparrow_bleu/urls.py
@@ -5,13 +5,13 @@ from apps.galleries.views import galleries
 
 urlpatterns = patterns('',
     url(r'^$', 'apps.sparrow_bleu.views.home', name='Home'),
-    url(r'^client-access/$', 'apps.sparrow_bleu.views.client_access', name='client_access'),
-    url(r'^login/$', 'apps.sparrow_bleu.views.user_login', name='user_login'),
-    url(r'^logout/$', 'apps.sparrow_bleu.views.user_logout', name='user_logout'),
+    url(r'^client-access', 'apps.sparrow_bleu.views.client_access', name='client_access'),
+    url(r'^login', 'apps.sparrow_bleu.views.user_login', name='user_login'),
+    url(r'^logout', 'apps.sparrow_bleu.views.user_logout', name='user_logout'),
 
-    url(r'^new-gallery/$', 'apps.galleries.views.new_gallery', name='new_gallery'),
-    url(r'^gallery-posted/$', 'apps.galleries.views.gallery_posted', name='gallery_posted'),
-    url(r'^galleries/$', 'apps.galleries.views.galleries', name='galleries'),
+    url(r'^new-gallery', 'apps.galleries.views.new_gallery', name='new_gallery'),
+    url(r'^gallery-posted', 'apps.galleries.views.gallery_posted', name='gallery_posted'),
+    url(r'^galleries', 'apps.galleries.views.galleries', name='galleries'),
     url(r'^gallery/(?P<pk>\d+)', 'apps.galleries.views.gallery_detail', name='gallery_detail'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
