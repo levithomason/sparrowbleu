abd5f321467204e3d4717276cfeed364ff502c2d
Levi Thomason <me@levithomason.com>
2013-11-04 00:31:59 -0800

    only flash on select

--- apps/galleries/static/js/gallery_detail.js
+++ apps/galleries/static/js/gallery_detail.js
@@ -15,10 +15,16 @@ $(window).load(function(){
 });
 
 $(document).ready(function() {
+
+    // selecting an image
     $('.gallery_image_item').click(function() {
         $(this).toggleClass('selected');
-        var thumb = $(this).find('.gallery_thumbnail')
-        thumb.hide();
-        thumb.fadeIn(800);
+
+        // flash the thumb if selected
+        if ($(this).hasClass('selected')) {
+            var thumb = $(this).find('.gallery_thumbnail')
+            thumb.hide();
+            thumb.fadeIn(800);
+        }
     });
 });
