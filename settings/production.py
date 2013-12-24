ef5fa93dd66424a0305d417ec430490e69745b64
Levi Thomason <me@levithomason.com>
2013-12-23 18:05:24 -0800

    debug on prod

--- settings/production.py
+++ settings/production.py
@@ -5,8 +5,8 @@ from default import *
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-DEBUG = False
-TEMPLATE_DEBUG = False
+DEBUG = True
+TEMPLATE_DEBUG = True
 
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
