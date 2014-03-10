ce0f290a79504828bed79fe332deb6b9975e888a
Levi Thomason <me@levithomason.com>
2014-03-09 22:28:08 -0700

    static config per heroku docs

--- settings/default.py
+++ settings/default.py
@@ -61,11 +61,11 @@ PROJECT_PATH = abspath(curdir)
 MEDIA_ROOT = '/medafiles/'
 MEDIA_URL = '/media/'
 
-STATIC_ROOT = '/staticfiles/'
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    './static/',
+    os.path.join(PROJECT_PATH, 'static'),
 )
 
 # List of finder classes that know how to find static files in
