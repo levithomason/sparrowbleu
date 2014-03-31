9f1dd84e113a7445ec453f79fc20e3955b78c243
Levi Thomason <me@levithomason.com>
2014-03-30 18:35:34 -0700

    fully working fullscreen with keyboard shortcuts

--- apps/galleries/static/js/gallery_detail.js
+++ apps/galleries/static/js/gallery_detail.js
@@ -1,96 +1,145 @@
 "use strict";
 
+// Selected images widget
+function update_selected_images() {
+    var widget_included = $('#selected_images .included'),
+        widget_extra = $('#selected_images .extra'),
+        max = widget_included.data('max'),
+        current = $('.gallery_image_item.selected').length,
+        cost_per_extra_image = $('#cost_per_extra_image').data('cost'),
+        remaining,
+        extra,
+        extra_text;
+
+    // update remaining images, min of 0
+    if (current <= max) {
+        remaining = max - current;
+    } else {
+        remaining = 0;
+    }
+
+    // update images extra
+    if (current > max) {
+        extra = Math.abs(max - current);
+    } else {
+        extra = 0;
+    }
+
+    // update the widget readout
+    if (extra > 0) {
+        extra_text = extra + " extra = $" + extra * cost_per_extra_image;
+
+        widget_included.removeClass('primary');
+
+        if (!widget_extra.hasClass('primary')) {
+            widget_extra.addClass('primary');
+        }
+
+    } else {
+        extra_text = extra + " extra";
+
+        widget_extra.removeClass('primary');
+
+        if (!widget_included.hasClass('primary')) {
+            widget_included.addClass('primary');
+        }
+    }
+
+    widget_included.text(remaining + " remaining");
+    widget_extra.text(extra_text);
+}
+
+
+function selectImage(image_element) {
+    var ele = $(image_element),
+        image_pk = ele.data('pk'),
+        thumb_overlay = ele.find('.gallery_thumbnail_overlay'),
+        these_images = $('[data-pk="' + image_pk + '"]'),
+        jqxhr;
+
+    thumb_overlay.show();
+    thumb_overlay.fadeOut(400);
+
+    jqxhr = $.post('/toggle-select-gallery-image/', {'image_pk': image_pk}, function() {
+        if (jqxhr.responseText === "True") {
+            these_images.addClass('selected');
+        } else {
+            these_images.removeClass('selected');
+        }
+
+        update_selected_images();
+    })
+        .fail(function() {
+            alert(
+                "Oops, couldn't change that image.  If this keeps happening, please contact SparrowBleu"
+            );
+        });
+}
+
 var fullscreen = {
-    'image_element': $('.fullscreen_view .image_container .image'),
+    'image_container': $('.fullscreen_view .image_container'),
+    'first_image': null,
+    'last_image': null,
     'previous_element': $('.fullscreen_view .controls .previous'),
     'next_element': $('.fullscreen_view .controls .next'),
-    'url_elements': $('#fullscreen_urls').find('.url'),
-    'urls': [],
     'currentImageIndex': null,
     'next': function() {
-        if (fullscreen.currentImageIndex === fullscreen.urls.length) {
-            fullscreen.currentImageIndex = 0;
-        } else {
-            fullscreen.currentImageIndex += 1;
-        }
+        var current = fullscreen.image_container.children('.active'),
+            next = current.next();
 
-        fullscreen.image_element.attr('src', fullscreen.urls[fullscreen.currentImageIndex]);
+        current.removeClass('active');
+
+        if (next.length) {
+            next.addClass('active');
+        } else {
+            fullscreen.first_image.addClass('active');
+        }
     },
     'previous': function() {
-        if (fullscreen.currentImageIndex === 0) {
-            fullscreen.currentImageIndex = fullscreen.urls.length;
-        } else {
-            fullscreen.currentImageIndex -= 1;
-        }
+        var current = fullscreen.image_container.children('.active'),
+            previous = current.prev();
 
-        fullscreen.image_element.attr('src', fullscreen.urls[fullscreen.currentImageIndex]);
+        current.removeClass('active');
+
+        if (previous.length) {
+            previous.addClass('active');
+        } else {
+            fullscreen.last_image.addClass('active');
+        }
     },
     'init': function() {
-        var urls = [],
-            i;
 
-        for (i = 0; i < fullscreen.url_elements.length; i++) {
-            urls[i] = $(fullscreen.url_elements[i]).text();
-        }
+        fullscreen.first_image = $('.fullscreen_view .image_container .fullscreen_image_item:first-child');
+        fullscreen.last_image = $('.fullscreen_view .image_container .fullscreen_image_item:last-child');
 
-        fullscreen.urls = urls;
+        fullscreen.first_image.addClass('active');
         fullscreen.currentImageIndex = 0;
-        fullscreen.image_element.attr('src', fullscreen.urls[fullscreen.currentImageIndex]);
+
+        // controls
+        $('body').on('keydown', function(e) {
+
+            // left
+            if (e.keyCode === 37) {
+                fullscreen.previous();
+            }
+
+            // right
+            if (e.keyCode === 39) {
+                fullscreen.next();
+            }
+
+            // space || enter
+            if (e.keyCode === 32 || 13) {
+                selectImage($('.fullscreen_view .image_container .fullscreen_image_item.active'));
+            }
+
+            console.log(e.keyCode);
+        });
     }
 };
 
 
 $(document).ready(function() {
-
-    // Selected images widget
-    function update_selected_images() {
-        var widget_included = $('#selected_images .included'),
-            widget_extra = $('#selected_images .extra'),
-            max = widget_included.data('max'),
-            current = $('.gallery_image_item.selected').length,
-            cost_per_extra_image = $('#cost_per_extra_image').data('cost'),
-            remaining,
-            extra,
-            extra_text;
-
-        // update remaining images, min of 0
-        if (current <= max) {
-            remaining = max - current;
-        } else {
-            remaining = 0;
-        }
-
-        // update images extra
-        if (current > max) {
-            extra = Math.abs(max - current);
-        } else {
-            extra = 0;
-        }
-
-        // update the widget readout
-        if (extra > 0) {
-            extra_text = extra + " extra = $" + extra * cost_per_extra_image;
-
-            widget_included.removeClass('primary');
-
-            if (!widget_extra.hasClass('primary')) {
-                widget_extra.addClass('primary');
-            }
-
-        } else {
-            extra_text = extra + " extra";
-
-            widget_extra.removeClass('primary');
-
-            if (!widget_included.hasClass('primary')) {
-                widget_included.addClass('primary');
-            }
-        }
-
-        widget_included.text(remaining + " remaining");
-        widget_extra.text(extra_text);
-
-    }
 
     // Image size controls
     $('.controls .thumbnails').click(function() {
@@ -110,31 +159,15 @@ $(document).ready(function() {
 
     // Selecting images
     $('.gallery_image_item').click(function() {
-        var ele = $(this),
-            image_pk = ele.data('pk'),
-            thumb_overlay = ele.find('.gallery_thumbnail_overlay'),
-            jqxhr;
+        selectImage(this);
+    });
 
-        thumb_overlay.show();
-        thumb_overlay.fadeOut(400);
-
-        jqxhr = $.post('/toggle-select-gallery-image/', {'image_pk': image_pk}, function() {
-            if (jqxhr.responseText === "True") {
-                ele.addClass('selected');
-            } else {
-                ele.removeClass('selected');
-            }
-
-            update_selected_images();
-        })
-            .fail(function() {
-                alert(
-                    "Oops, couldn't change that image.  If this keeps happening, please contact SparrowBleu"
-                );
-            });
+    $('.fullscreen_image_item').click(function() {
+        selectImage(this);
     });
 
 
+    // Init
     function init() {
         update_selected_images();
         $('.controls .thumbnails').click();
