ba5522a2aa32310d4aef5ff00efa7fce9a610e59
ckcollab <eric@ckcollab.com>
2014-04-23 04:27:48 -0700

    adds awesome deploy stuff woo!

--- fabfile.py
+++ fabfile.py
@@ -1,13 +1,29 @@
-from fabric.api import local
+import sys
+from fabric.api import local, hide
+
+def test():
+    local('python manage.py test --attr=!e2e')
 
 def test_e2e():
     local('python manage.py test --attr=e2e --nologcapture')
 
 def deploy():
-    # run tests
-    # run e2e tests
-    # run jasmine tests
-    # run pep8
-    # git push
-    # git push heroku master
-    pass
+    with hide('stdout'):
+        print "%" * 80
+        print " Deploying Sparrow Bleu, me lord!"
+        print "%" * 80
+        print ""
+
+        with hide('running', 'output', 'stdout'):
+            _fancy_output("Running python unit tests", test)
+            _fancy_output("Running python E2E tests", test_e2e)
+
+            # run jasmine tests
+            # run pep8
+            # git push
+            # git push heroku master
+
+def _fancy_output(message, func):
+    sys.stdout.write("%s..." % message)
+    func()
+    print "done"
