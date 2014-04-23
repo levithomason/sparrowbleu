d79805db72b5fe40a763d4914fa1184452dec469
ckcollab <eric@ckcollab.com>
2014-04-23 04:15:48 -0700

    adds outline for deploy

--- fabfile.py
+++ fabfile.py
@@ -2,3 +2,12 @@ from fabric.api import local
 
 def test_e2e():
     local('python manage.py test --attr=e2e --nologcapture')
+
+def deploy():
+    # run tests
+    # run e2e tests
+    # run jasmine tests
+    # run pep8
+    # git push
+    # git push heroku master
+    pass
