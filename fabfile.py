51ec8e116cdb9459fc2c5b0690a946bdb00e7445
ckcollab <eric@ckcollab.com>
2014-04-23 04:14:57 -0700

    woo got e2e tests running!

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,4 @@
 from fabric.api import local
 
 def test_e2e():
-    # This will run all E2E tests... when it works, for some reason it doesn't find any of the E2E tests
-    #local('python manage.py test -a e2e --nologcapture')
-    local('python manage.py test sparrow_bleu --nologcapture')
+    local('python manage.py test --attr=e2e --nologcapture')
