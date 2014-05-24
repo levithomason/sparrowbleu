491a555d2bdd0015b7f7c3ec205e92c866a585c1
Levi Thomason <me@levithomason.com>
2014-05-24 02:43:58 -0700

    client access button loading state

--- apps/galleries/static/js/client_access.js
+++ apps/galleries/static/js/client_access.js
@@ -1,1 +1,10 @@
 jQuery('input[name="is_mobile"]').prop('checked', isMobile.any);
+
+$('[type="submit"]').on('click', function() {
+    setTimeout(function() {
+        $('[type="submit"]')
+            .addClass('disabled')
+            .attr("disabled", "disabled")
+            .html('<i class="fa fa-spinner fa-spin"></i> Fetching');
+    });
+});
